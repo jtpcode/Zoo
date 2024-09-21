77939e0b14aa4396fec9b8755895d1890a7b8a90
jtpcode <152286055+jtpcode@users.noreply.github.com>
2024-09-21 13:39:11 +0300

    Create user functionality added

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Zoo
-The idea of this application is to serve the staff of an imaginary Zoo. Once logged in a member of the staff can manipulate the database to for example:
+The idea of this application is to serve the staff of an imaginary Zoo. Once logged in a member of the staff can manipulate the database 
+to for example:
 - add new animals (species, origin, name, birthday, special diet, health status...)
 - search information about animals based on name, species, origin..
 - add a message (with timestamp and who wrote the message) concerning animal care, for example "Ben the bear was fed and given antibiotics"
@@ -8,4 +9,25 @@ The idea of this application is to serve the staff of an imaginary Zoo. Once log
 - add new staff members as users to the application
 - ...
 
-The app could include also for example food/supply stock etc. but it might may get too large for the course purposes?
+The app could include also for example food/supply stock etc.
+
+How to use:
+
+Kloonaa tämä repositorio omalle koneellesi ja siirry sen juurikansioon. Luo kansioon .env-tiedosto ja määritä sen sisältö seuraavanlaiseksi:
+
+DATABASE_URL=<tietokannan-paikallinen-osoite>
+SECRET_KEY=<salainen-avain>
+
+Seuraavaksi aktivoi virtuaaliympäristö ja asenna sovelluksen riippuvuudet komennoilla:
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r ./requirements.txt
+
+Määritä vielä tietokannan skeema komennolla:
+$ psql < schema.sql
+
+Nyt voit käynnistää sovelluksen komennolla:
+$ flask run
+
+
+At the moment you can create a new user from the homepage to enable easy testing. Database allows admin users, but for now new users are basic users by default and no admin features have been implemented. If this was a real application only admins could add new users, which would require adding admin user manually into the database.
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
@@ -10,17 +10,19 @@ app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
-# Show "wrong user/password" -notification or not
-fresh_start = True
-
 @app.route("/")
 def index():
-    return render_template("index.html", fresh_start=fresh_start) 
+    login_ok_str = request.args.get("login_ok", default="True")
+    # Change str -> bool
+    login_ok = login_ok_str == "True"
+
+    return render_template("index.html", login_ok=login_ok) 
 
 @app.route("/login",methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
+    login_ok = False
 
     # check username and password
     sql = "SELECT id, password FROM users WHERE username=:username"
@@ -32,14 +34,42 @@ def login():
         if check_password_hash(hash_value, password):
             # correct password
             session["username"] = username
-            fresh_start = True
-            
-    if "username" not in session:
-        fresh_start = False
+            login_ok = True
 
-    return render_template("index.html", fresh_start=fresh_start)
+    return redirect(url_for("index", login_ok=login_ok))
 
 @app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
+
+@app.route("/create_user")
+def create_user():
+    password_ok_str = request.args.get("password_ok", default="True")
+    # Change str -> bool
+    password_ok = password_ok_str == "True"
+
+    return render_template("create_user.html", password_ok=password_ok)
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+
+    # check password
+    if password1 == password2:
+        hash_value = generate_password_hash(password1)
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(text(sql), {"username":username, "password":hash_value})
+        db.session.commit()
+
+        return redirect(url_for("user_added"))
+    else:
+        password_ok = False
+        return redirect(url_for("create_user", password_ok=password_ok))
+
+@app.route("/user_added")
+def user_added():
+    return "<p>Käyttäjätunnus luotu onnistuneesti!</p><a href='/'>Siirry kirjautumissivulle</a>"
+
